64d8fcfa2cce6b4ad570a24389bc9fcfc53f6ed0
Naman Luthra <namanluthra@Namans-MacBook-Air.local>
2022-07-16 01:05:44 +0530

    initial commit

--- src/app/store.js
+++ src/app/store.js
@@ -1,8 +1,10 @@
 import { configureStore } from '@reduxjs/toolkit';
-import counterReducer from '../features/counter/counterSlice';
+import authReducer from '../features/auth/authSlice';
+import doubtsReducer from '../features/doubts/doubtsSlice';
 
 export const store = configureStore({
   reducer: {
-    counter: counterReducer,
+    auth: authReducer,
+    doubts: doubtsReducer,
   },
 });
--- src/index.js
+++ src/index.js
@@ -2,22 +2,34 @@ import React from 'react';
 import { createRoot } from 'react-dom/client';
 import { Provider } from 'react-redux';
 import { store } from './app/store';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { App } from './App';
 import './index.css';
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+} from "react-router-dom";
+import { PrivateRoute } from './features/auth/PrivateRoute';
+import { SignIn } from './features/auth/SignIn';
+import { SignUp } from './features/auth/SignUp';
+import { RaiseDoubt } from './features/pages/RaiseDoubt';
+import { SolveDoubts } from './features/pages/SolveDoubts';
+import { SolveDoubt } from './features/pages/SolveDoubt';
 
 const container = document.getElementById('root');
 const root = createRoot(container);
 
 root.render(
-  <React.StrictMode>
-    <Provider store={store}>
-      <App />
-    </Provider>
-  </React.StrictMode>
+  <Provider store={store}>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<PrivateRoute><App /></PrivateRoute>} />
+        <Route path="/ask-doubt" element={<PrivateRoute><RaiseDoubt /></PrivateRoute>} />
+        <Route path="/solve-doubts" element={<PrivateRoute><SolveDoubts /></PrivateRoute>} />
+        <Route path="/solve-doubt/:doubtId" element={<PrivateRoute><SolveDoubt /></PrivateRoute>} />
+        <Route path="/signin" element={<SignIn />} />
+        <Route path="/signup" element={<SignUp />} />
+      </Routes>
+  </BrowserRouter>
+  </Provider>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
